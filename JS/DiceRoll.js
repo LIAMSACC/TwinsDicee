9324fbfe586fb46d4b3a62bd5083e6bacf60cbd6
LIAMSACC <LIAMFAWCETT@HOTMAIL.COM>
2020-07-28 18:58:47 +0100

    Add files via upload

---
+++ JS/DiceRoll.js
@@ -0,0 +1,18 @@
+ // alert ("working");
+ var randomNumber1 = Math.floor(Math.random()*6)+1;
+ var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+//assign the left image to the randomNumber1
+var dice1Image = document.querySelectorAll("img")[0].setAttribute("src","Images/Dice"+randomNumber1+".png");
+//assign to the right too:
+var diceImage2 = document.querySelectorAll("img")[1].setAttribute("src","Images/Dice"+randomNumber2+".png");
+//calculate the value to assign the h1 result
+if (randomNumber1>randomNumber2) {
+  document.querySelector("h1").innerHTML="🐼 Ellis Wins!";
+}
+  else if (randomNumber2>randomNumber1) {
+    document.querySelector("h1").innerHTML="🐼 Esmé Wins!";
+  }
+  else {
+    document.querySelector("h1").innerHTML="Draw";
+  }
