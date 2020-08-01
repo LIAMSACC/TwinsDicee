3a1bffe2325f56d402f07dc3bfae5beb7ad0d449
LIAMSACC <LIAMFAWCETT@HOTMAIL.COM>
2020-08-01 13:19:07 +0100

    Update DiceRoll.js

--- JS/DiceRoll.js
+++ JS/DiceRoll.js
@@ -3,7 +3,7 @@
  var randomNumber2 = Math.floor(Math.random()*6)+1;
 
 //assign the left image to the randomNumber1
-var dice1Image = document.querySelectorAll("img")[0].setAttribute("src","images/Dice"+randomNumber1+".png");
+var dice1Image = document.querySelectorAll("img")[0].setAttribute("src","images/dice"+randomNumber1+".png");
 //assign to the right too:
 var diceImage2 = document.querySelectorAll("img")[1].setAttribute("src","images/Dice"+randomNumber2+".png");
 //calculate the value to assign the h1 result
